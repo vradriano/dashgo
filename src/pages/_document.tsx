565f08f4ac414bcd3bad2525ad3739803aa1fdbb
unknown <vradriano@hotmail.com>
2022-05-14 22:01:33 -0300

    Adicionando typescript, configurando chakraUI e configurando fonte

---
+++ src/pages/_document.tsx
@@ -0,0 +1,19 @@
+import Document, { Html, Main, NextScript } from 'next/document'
+import Head from 'next/head'
+
+export default class myDocument extends Document {
+  render () {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
