a2cb0bb6a4560cd109db49b0656daee9aada7bff
unknown <vradriano@hotmail.com>
2022-06-02 23:48:14 -0300

    check if development mode, add miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
+import { makeServer } from '../services/mirage/index'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
