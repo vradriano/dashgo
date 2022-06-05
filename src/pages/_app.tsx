4659002244e1528d8777432e49260d77ab91eb6b
unknown <vradriano@hotmail.com>
2022-06-04 22:18:03 -0300

    Add react-query, useQuery fetching fakee api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,24 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 import { makeServer } from '../services/mirage/index'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
+import { QueryClientProvider, QueryClient } from 'react-query'
+
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
 
+const queryClient = new QueryClient()
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider resetCSS theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,22 +1,25 @@
-import Header from '../../components/Header'
-import Sidebar from '../../components/Sidebar'
-import Pagination from '../../components/Pagination'
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react'
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react'
 import { RiAddLine, RiPencilLine } from "react-icons/ri"
 import Link  from 'next/link'
 import { useEffect } from 'react'
+import { useQuery } from 'react-query'
+
+import Header from '../../components/Header'
+import Sidebar from '../../components/Sidebar'
+import Pagination from '../../components/Pagination'
 
 export default function UserList() {
+  const { data, isLoading, isError } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+    return data
+  })
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   })
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users').then(response => response.json()).then(data => console.log(data))
-  }, [])
-
 
   return (
     <Box>
@@ -42,45 +45,57 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                { isWideVersion && ( <Td> Data de cadastro</Td> )}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td  px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold" >Vitor Ricardo</Text>
-                    <Text fontSize="small" color="gray.300" >vradriano@hotmail.com</Text>
-                  </Box>
-                </Td>
-               { isWideVersion && ( <Td>19 de maio de 2022</Td> )}
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="small"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-
-          <Pagination />
+          
+          {
+            isLoading ? (
+              <Flex justify="center">
+                <Spinner />
+              </Flex>
+            ) : isError ? (
+              <Flex justify="center">Falha ao obter dados dos usuários</Flex>
+            ) : (
+              <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    { isWideVersion && ( <Td> Data de cadastro</Td> )}
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td  px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold" >Vitor Ricardo</Text>
+                        <Text fontSize="small" color="gray.300" >vradriano@hotmail.com</Text>
+                      </Box>
+                    </Td>
+                  { isWideVersion && ( <Td>19 de maio de 2022</Td> )}
+                    <Td>
+                      <Button
+                        as="a"
+                        size="sm"
+                        fontSize="small"
+                        colorScheme="purple"
+                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                      >
+                        Editar
+                      </Button>
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            <Pagination />
+            </>
+            )
+          }
         </Box>
 
 
